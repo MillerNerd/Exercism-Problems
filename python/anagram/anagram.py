e2f9493006e17eb96796f174d51d2c5149b4dcbf
Sean <millernerd@gmail.com>
2020-07-20 22:26:49 -0500

    Converted to list comprehension

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,7 +1,3 @@
 def find_anagrams(word, candidates):
     answer = []
-    for i in candidates:
-        if sorted(word.lower()) == sorted(i.lower()):
-            if word.lower() != i.lower():
-                answer.append(i)
-    return answer
+    return [i for i in candidates if sorted(word.lower()) == sorted(i.lower()) and word.lower() != i.lower()]
