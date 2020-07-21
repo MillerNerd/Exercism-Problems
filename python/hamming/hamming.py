6853e9fedd5dd63c1cf35e4dbb97f8b9f494b192
Sean <millernerd@gmail.com>
2020-07-20 22:02:58 -0500

    Solved hamming

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,2 +1,4 @@
 def distance(strand_a, strand_b):
-    pass
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Unequal Lengths")
+    return sum(1 for a, b in zip(strand_a, strand_b) if a != b)
