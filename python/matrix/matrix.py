45c37775fa79f0ec1ca6187281a8abbd7428ddea
Sean <millernerd@gmail.com>
2020-07-20 21:35:39 -0500

    Removed unnecessary self-references in __init__

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -4,9 +4,9 @@ import numpy as np
 class Matrix:
     def __init__(self, matrix_string):
         self.arr2d = []
-        self.arr1d = matrix_string.split('\n')
-        for i in range(len(self.arr1d)):
-            self.arr2d.append(list(map(int, self.arr1d[i].split())))
+        arr1d = matrix_string.split('\n')
+        for i in range(len(arr1d)):
+            self.arr2d.append(list(map(int, arr1d[i].split())))
 
     def row(self, index):
         return self.arr2d[index-1]
