b524eec6210c5f4f34ade7d13a260b87f82b983a
Sean <millernerd@gmail.com>
2020-07-28 21:34:31 -0500

    initialized dict

--- python/twelve-days/twelve_days.py
+++ python/twelve-days/twelve_days.py
@@ -1,2 +1,17 @@
+from typing import Dict
+
+
 def recite(start_verse, end_verse):
-    pass
+    days = {1: ("first", "a Partridge in a Pear Tree."),
+            2: ("second", "two Turtle Doves, "),
+            3: ("third", "three French Hens, "),
+            4: ("fourth", "four Calling Birds, "),
+            5: ("fifth", "five Gold Rings, "),
+            6: ("sixth", "six Geese-a-Laying, "),
+            7: ("seventh", "seven Swans-a-Swimming, "),
+            8: ("eighth", "eight Maids-a-Milking, "),
+            9: ("ninth", "nine Ladies Dancing, "),
+            10: ("tenth", "ten Lords-a-Leaping, "),
+            11: ("eleventh", "eleven Pipers Piping, "),
+            12: ("twelfth", "twelve Drummers Drumming, ")}
+    return ["On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree."]
