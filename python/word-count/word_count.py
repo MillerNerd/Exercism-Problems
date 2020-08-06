86931556f7fa9f934a79e3654bd51bcac6c39e9a
Sean <millernerd@gmail.com>
2020-08-06 00:11:14 -0500

    Mostly working

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -1,3 +1,8 @@
+import collections
+import re
+
+
 def count_words(sentence):
-    print(sentence)
-    pass
+    print('\n', sentence, '\n')
+    print((list(filter(None, re.split('[\s,.?!&@$%^&:_]', sentence.lower())))))
+    return collections.Counter((list(filter(None, re.split('[\s,.?!&@$%^&:_]', sentence.lower())))))
