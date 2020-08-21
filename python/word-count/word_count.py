8827bb825faa90c81b37567e9d5cb8542a0a6cd6
Sean <millernerd@gmail.com>
2020-08-21 15:21:34 -0500

    Expanded single-line answer to make more readable. Commented.

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -3,4 +3,14 @@ import re
 
 
 def count_words(sentence):
-    return collections.Counter([a.strip("'") for a in filter(None, re.split('[\s,.?!&@$%^&:_]', sentence.lower()))])
+    # make lowercase because case-insensitive (and answers look for lowercase)
+    answer = sentence.lower()
+    # remove characters we don't care about and split into a list of words delimited by characters/whitespace
+    # we will get to apostrophe later, because contractions
+    answer = re.split('[\s,.?!&@$%^&:_]', answer)
+    # remove empty strings in list left by re.split
+    answer = filter(None, answer)
+    # strip apostrophes, which leaves contractions/possessives but removes single-quotes
+    answer = [a.strip("'") for a in answer]
+    # count up matching words, returns dict
+    return collections.Counter(answer)
