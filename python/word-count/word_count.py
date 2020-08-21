1a358f84e2e55b4091075e164f0c9c9785ddf09a
Sean <millernerd@gmail.com>
2020-08-21 14:32:22 -0500

    Removed unnecessary list()

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -3,4 +3,4 @@ import re
 
 
 def count_words(sentence):
-    return collections.Counter([a.strip("'") for a in list(filter(None, re.split('[\s,.?!&@$%^&:_]', sentence.lower())))])
+    return collections.Counter([a.strip("'") for a in filter(None, re.split('[\s,.?!&@$%^&:_]', sentence.lower()))])
