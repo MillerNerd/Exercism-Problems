359baea8cbee09ebc19eaccb91401bb46bb08574
Sean <millernerd@gmail.com>
2021-04-28 18:18:44 -0500

    Removed comments; implemented generator expressions

--- python/word-count/word_count.py
+++ python/word-count/word_count.py
@@ -3,14 +3,8 @@ import re
 
 
 def count_words(sentence):
-    # make lowercase because case-insensitive (and answers look for lowercase)
     answer = sentence.lower()
-    # remove characters we don't care about and split into a list of words delimited by characters/whitespace
-    # we will get to apostrophe later, because contractions
     answer = re.split('[\s,.?!&@$%^&:_]', answer)
-    # remove empty strings in list left by re.split
-    answer = filter(None, answer)
-    # strip apostrophes, which leaves contractions/possessives but removes single-quotes
-    answer = [a.strip("'") for a in answer]
-    # count up matching words, returns dict
+    answer = (x for x in answer if x is not '')
+    answer = (a.strip("'") for a in answer)
     return Counter(answer)
